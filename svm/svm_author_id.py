9e94f44328485f0aaa0edf584516f4e9a90ae298
pmeshkat <pmeshkat@gmail.com>
2017-01-25 13:30:17 -0800

    See effect of C on acuracy with rbf

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -18,8 +18,20 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+from sklearn.svm import SVC
+# clf = SVC(kernel="linear")
+for C_val in [10,100,1000,10000]:
+	clf = SVC(kernel="rbf", C=C_val)
 
+	clf.fit(features_train,labels_train)
+	labels_predict=clf.predict(features_test)
 
+	from sklearn.metrics import accuracy_score
+	print  'C is:', C_val, 'Accuracy is: ', accuracy_score(labels_predict,labels_test)
+
+# clf = SVC(kernel="rbf",C=10000)
 
 
 #########################################################
